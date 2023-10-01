582218a3e52f2ea2c074b23919fd623184ef0f85
Arthur Rocha <145076571+Arthurr-Rosante@users.noreply.github.com>
2023-10-01 17:03:18 -0300

    Criação e Estilização do Form
    - Codigo html do form criado
    - Codigo css do form criado
    - Novo arquivo Js criado
    - Funções Js do form parcialmente criadas
    -imagem do form adicionada

---
+++ assets/js/logarCadastro.js
@@ -0,0 +1,13 @@
+
+const frm = document.querySelector("form");
+const username = document.querySelector(""); //atribuir a tag correta no futuro
+
+frm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const nomeUser = frm.inUser.value;
+    //***.innerHTML=`User: ${nomeUser}`
+    
+})
+
